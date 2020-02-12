bd1fc8ad8c6953657eea8fa0452718fc5ac864bd
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    MOD: msm8937-common: Enable freeform windows
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: TTTT55 <iamtttt55@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
