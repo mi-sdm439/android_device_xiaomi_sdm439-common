3d1b3d9ab6107fce4bd663163e3e4511d85aa92c
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-15 01:17:11 +0800

    sdm439-common: init: Kang hardware variant setting functions from sm8250-common

--- init/include/libinit_sdm439.h
+++ init/include/libinit_sdm439.h
@@ -31,7 +31,23 @@
 #ifndef LIBINIT_SDM439_H
 #define LIBINIT_SDM439_H
 
+#include <string>
+#include <vector>
+
+typedef struct variant_info {
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string build_description;
+    std::string build_fingerprint;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+void set_variant_props(const variant_info_t variant);
+
 void property_override(char const prop[], char const value[], bool add = true);
 void set_dalvik_heap_size();
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);
 
 #endif
--- init/libinit_sdm439.cpp
+++ init/libinit_sdm439.cpp
@@ -45,6 +45,15 @@
 
 using android::base::GetProperty;
 
+std::vector<std::string> ro_props_default_source_order = {
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "",
+};
+
 void property_override(char const prop[], char const value[], bool add)
 {
     auto pi = (prop_info *) __system_property_find(prop);
@@ -53,6 +62,19 @@ void property_override(char const prop[], char const value[], bool add)
         __system_property_update(pi, value, strlen(value));
     } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product) {
+    std::string prop_name;
+
+    for (const auto &source : ro_props_default_source_order) {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str(), true);
     }
 }
 
@@ -101,4 +123,15 @@ void set_dalvik_heap_size()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
+void set_variant_props(const variant_info_t variant) {
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("model", variant.model, true);
+
+    set_ro_build_prop("fingerprint", variant.build_fingerprint);
+    property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint.c_str());
+    property_override("ro.build.description", variant.build_description.c_str());
 }
