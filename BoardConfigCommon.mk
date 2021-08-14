bbefc17aef52ad5f62c8c9a043c128f06786641c
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-15 01:00:13 +0800

    sdm439-common: init: Convert to helper libinit
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,10 +116,6 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 # HW crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sdm439
-TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm439
-
 # Partitions
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
