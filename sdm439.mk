e01f24d5d763b0feb61ea43620f0ec863405b424
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    sdm439-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm439.mk
+++ sdm439.mk
@@ -310,6 +310,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
