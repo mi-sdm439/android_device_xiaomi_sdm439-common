1f1a050995f85066ba99729610b0f544a0d9c975
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-30 15:47:55 +0800

    RevengeOS: sdm439-common: Pick 720 bootanim res

--- sdm439.mk
+++ sdm439.mk
@@ -383,5 +383,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# ROM: RevengeOS
+ifneq ($(wildcard vendor/revengeos),)
+TARGET_BOOT_ANIMATION_RES := 720
+endif
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm439-common/sdm439-common-vendor.mk)
