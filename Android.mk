ae0819c8f50eb3f8cffcde2893f40f4e877f4ea8
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-01-02 17:58:52 +0800

    sdm439-common: Welcome mi439

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter pine olives,$(TARGET_DEVICE)),)
+ifneq ($(filter mi439 pine olives,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
