e4f9226a7efb6fd3846bb0e4bc5f09f4c7d27d56
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-01-03 13:08:23 +0800

    sdm439-common: Update thermal-engine from olive

--- sdm439.mk
+++ sdm439.mk
@@ -307,10 +307,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
