111d7fefc02938045c935019315a5704a2cecb7f
Albert I <kras@raphielgang.org>
2022-01-21 21:49:46 +0800

    msm8937-common: Enable GRF/VF for all devices
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,9 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
+# GRF/VF
+BOARD_SHIPPING_API_LEVEL := 30
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
