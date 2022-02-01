d7e26bafa3051843a58cd8d4ed1c6425bd8bc08b
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-02-01 15:44:11 +0800

    sdm439-common: Remove QTI cryptfshw

--- sdm439.mk
+++ sdm439.mk
@@ -187,10 +187,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
