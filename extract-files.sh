ea6aaafb2ea942326f34e2db8e405d9a874294a3
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-03 20:21:59 +0800

    sdm439-common: Skip fixup for daisy camera lib
    This change lets the extraction script to continue when extracting blobs from daisy

--- extract-files.sh
+++ extract-files.sh
@@ -101,6 +101,10 @@ function blob_fixup() {
             sed -i 's|3|4|g' "${2}"
             ;;
         vendor/lib/libmmcamera2_sensor_modules.so)
+            if grep -q "sakura_" "${2}"; then
+                echo "Daisy camera blob detected, skipping fixup"
+                return
+            fi
             # Allow up to 0xFF CameraModuleConfig nodes on camera_config.xml
             sed -i -e 's|\x68\x1e\x15\x28|\x68\x1e\xff\x28|g' "${2}"
             PATTERN_FOUND=$(hexdump -ve '1/1 "%.2x"' "${2}" | grep -E -o "681eff28" | wc -l)
